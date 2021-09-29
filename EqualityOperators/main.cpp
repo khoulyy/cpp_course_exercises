a16edd3cdc7e7be1ea94305070f7a759b89bdc31
Khouly <abdoelkhouly555@gmail.com>
2021-09-29 15:07:12 +0200

    Added new example for equality-operator example

--- EqualityOperators/main.cpp
+++ EqualityOperators/main.cpp
@@ -7,7 +7,7 @@ int main()
     bool equal_result {false};
     bool not_equal_result {false};
 
-    int num1 {}, num2 {};
+//    int num1 {}, num2 {};
 
      cout << boolalpha; // will display true/false instead of 1/0 for booleans
 
@@ -15,23 +15,24 @@ int main()
 //    cin >> num1 >> num2;
 //    equal_result = (num1 == num2);
 //    not_equal_result = (num1 != num2);
-//    cout << "Comparison result (equals): " << equal_result << endl;
-//    cout << "Comparison result (not equals): " << not_equal_result << endl;
+//    cout << "Comparision result (equals): " << equal_result << endl;
+//    cout << "Comparision result (not equals): " << not_equal_result << endl;
 
-    char char1 {}, char2 {};
-    cout << "Enter two characters separated by a space: ";
-    cin >> char1 >> char2;
-    equal_result = (char1 == char2);
-    not_equal_result = (char1 != char2);
+//    char char1 {}, char2 {};
+//    cout << "Enter two characters separated by a space: ";
+//    cin >> char1 >> char2;
+//    equal_result = (char1 == char2);
+//    not_equal_result = (char1 != char2);
+//    cout <<"Comparision result (equals): " << equal_result << endl;
+//    cout << "Comparision result (not equals): " << not_equal_result << endl;
 
-    // cout << boolalpha; // will display true/false instead of 1/0 for booleans
+    double double1 {}, double2 {};
+    cout << "Enter two doubles separated by a space: ";
+    cin >> double1 >> double2;
+    equal_result = (double1 == double2);
+    not_equal_result = (double1 != double2);
+    cout << "Comparision result (equals): " << equal_result << endl;
+    cout << "Comparision result (not equals): " << not_equal_result << endl;
 
-    cout << "Enter two integers separated by spaces: ";
-    cin >> num1 >> num2;
-    equal_result = (num1 == num2);
-    not_equal_result = (num1 != num2);
-
-    cout << "Comparison result (equals): " << equal_result << endl;
-    cout << "Comparison result (not equals): " << not_equal_result << endl;
     return 0;
 }
