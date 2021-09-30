a09c4fd139d7d183e6f69d984232327647b37766
Khouly <abdoelkhouly555@gmail.com>
2021-09-30 19:30:33 +0200

    Added LogicalOperators example

---
+++ LogicalOperators/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num {};
+    const int lower {10};
+    const int upper {20};
+
+    cout << boolalpha;
+
+//    cout << "Enter an integer - the bounds are " << lower << " and " << upper << " : ";
+//    cin >> num;
+//
+//    bool within_bounds {false};
+//
+//    within_bounds = (num > lower && num < upper);
+//    cout << num << " is between " << lower << " and " << upper << " : " << within_bounds << endl;
+
+//    cout << "Enter and integer - the bounds are " << lower << " and " << upper << " : ";
+//    cin >> num;
+//
+//    bool outside_bounds {false};
+//    outside_bounds = (num < lower || num > upper);
+//    cout << num << " is outside " << lower << " and " << upper << " : " << outside_bounds << endl;
+
+//    cout << "Enter an integer - the bounds are " << lower << " and " << upper << " : ";
+//    cin >> num;
+//
+//    bool on_bounds {false};
+//    on_bounds = (num == lower || num == upper);
+//    cout << num << " is on one of the bounds which are " << lower << " and " << upper << " : " << on_bounds << endl;
+
+    return 0;
+}
